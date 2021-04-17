d30c0af7e6d33c690d883b4d218cc573ec5ccfa4
Yuki <yukipooh4120@gmail.com>
2021-04-16 17:48:05 -0700

    Meal Maker Project done

--- Week1/mealMaker.js
+++ Week1/mealMaker.js
@@ -1,4 +1,5 @@
 //Instruction:
+//Meal Maker Project
 //In this project, you’ll use JavaScript to randomly create a three-course meal 
 //based on what is available on a menu. We’ll keep running it until we’re satisfied with the generated meal!
 
